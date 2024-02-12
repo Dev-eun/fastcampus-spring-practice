218f1003f9cd87afda1d5ec81ce4bc8faed60cf5
박하은 <deveun23@gmail.com>
2024-02-12 23:34:59 +0900

    Initialize #1 - java project
    지난 시간에 만들었던 것을 복사해 왔다.
    
    * https://github.com/Dev-eun/fastcampus-java-oop

---
+++ src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.fastcampus.javaoop.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort <T extends Comparable<T>> implements Sort<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        // 입력과 출력의 메모리를 구분 -> 출력에 변화를 줘도 입력에 영향이 가지 않음
+        List<T> output = new ArrayList<>(list);
+
+        for (int i = output.size() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (output.get(j).compareTo(output.get(j + 1)) > 0) {
+                    T temp = output.get(j);
+                    output.set(j, output.get(j + 1));
+                    output.set(j + 1, temp);
+                }
+            }
+        }
+
+        return output;
+    }
+}
